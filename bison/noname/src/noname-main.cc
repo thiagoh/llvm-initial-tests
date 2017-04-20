cb1c450c71383689782a39990fd012a89fd5b613
Thiago Andrade <thiagoh@gmail.com>
2017-04-20 16:55:30 -0400

    compiling with clang++

--- bison/noname/src/noname-main.cc
+++ bison/noname/src/noname-main.cc
@@ -6,26 +6,8 @@
 #include <stdlib.h>
 #include <string>
 
-// void yyerror(const char *);
-
-// #define YY_NO_UNPUT /* keep g++ happy */
-// extern FILE *fin;   /* we read from this file */
-
-// // /* define YY_INPUT so we read from the FILE fin:
-// // * This change makes it possible to use this scanner in
-// // * the Cool compiler.
-// // */
-// #undef YY_INPUT
-// #define YY_INPUT(buf, result, max_size)                                        \
-//   if ((result = fread((char *)buf, sizeof(char), max_size, fin)) < 0)          \
-//     YY_FATAL_ERROR("read() in flex scanner failed");
-
-// extern int curr_lineno;
-// extern int verbose_flag;
-
 std::map<int, std::string> map;
 extern YYSTYPE yylval;
-// extern int yylex(void);
 extern int noname_yylex(void);
 extern int yyparse();
 void yyerror(char const *s);
@@ -37,41 +19,6 @@ void yyerror(char const *s);
 int curr_lineno = 1;
 char *curr_filename = "<stdin>";  // this name is arbitrary
 // FILE *fin; // This is the file pointer from which the lexer reads its input.
-
-//
-//  noname_yylex() is the function produced by flex. It returns the next
-//  token each time it is called.
-//
-// #define YY_DECL extern int noname_yylex()
-
-// #define YY_DECL int yylex()
-// #define YY_DECL int noname_yylex(YYSTYPE * yylval_param , yyscan_t yyscanner)
-// extern int noname_yylex(YYSTYPE * yylval_param , yyscan_t yyscanner);
-// YYSTYPE noname_yylval; // Not compiled with parser, so must define this.
-
-// extern int optind; // used for option processing (man 3 getopt for more info)
-
-//
-//  Option -v sets the lex_verbose flag. The main() function prints out tokens
-//  if the program is invoked with option -v.  Option -l sets yy_flex_debug.
-//
-// extern int yy_flex_debug; // Flex debugging; see flex documentation.
-// extern int lex_verbose;   // Controls printing of tokens.
-// void handle_flags(int argc, char *argv[]);
-
-//
-//  The full Cool compiler contains several debugging flags, all of which
-//  are handled and set by the routine handle_flags.  Here we declare
-//  noname_yydebug, which is not used by the lexer but is needed to link
-//  with handle_flags.
-//
-// int noname_yydebug;
-
-// defined in utilities.cc
-// extern void dump_noname_token(ostream &out, int lineno, int token, YYSTYPE
-// yylval);
-
-/* Called by yyparse on error.  */
 
 int yylex(void) {
   int token = noname_yylex();
